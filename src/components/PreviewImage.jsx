64da6f8cfc773b75cf71db7658146d87dfc0ab2f
sandeepptpss <sandeepptpss@gmail.com>
2023-04-01 01:59:56 +0530

    image

---
+++ src/components/PreviewImage.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+function PreviewImg(){
+
+const [file, setFile] = useState([]);
+
+function handleChange(e) {
+
+
+
+var element = document.getElementById("myDIV");
+element.classList.add("active");
+setFile(URL.createObjectURL(e.target.files[0]));
+
+
+
+localStorage.setItem('src',URL.createObjectURL(e.target.files[0]));
+
+
+var data = localStorage.getItem("src")
+alert(data)
+}
+return(
+    <div className="App">
+        <h2>Display Preview Image  </h2>
+        <input type="file" onChange={handleChange} />
+    
+        <img id="myDIV" class="preview-img"  src={file}  alt="Previw Img"/>
+    </div>
+);
+}
+export default PreviewImg;
+
+ 
+
