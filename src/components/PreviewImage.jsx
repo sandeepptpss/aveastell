000b80e1ec615635ba8df3841217d83ed5eb3944
sandeepptpss <sandeepptpss@gmail.com>
2023-04-04 23:58:23 +0530

    update priview code

--- src/components/PreviewImage.jsx
+++ src/components/PreviewImage.jsx
@@ -1,26 +1,54 @@
 import React, { useState,useRef } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import preview from '../assets/images/preview.webp'; 
+
 function PreviewImg(){
+
+  // useState Hook 
+
 const ref = useRef();
 const [File, setFile] = useState([]);
-function handleChange(e) {
+
+// end useState Hook 
+function handleChange(e){
 setFile(URL.createObjectURL(e.target.files[0]));
 localStorage.setItem('File',URL.createObjectURL(e.target.files[0]));
 }
-console.log(File)
-const remove = () => {
+
+const remove = ()=>{
      setFile();
      ref.current.value = "";
-localStorage.removeItem("File");
+     localStorage.removeItem("File");
 };
+ const image_path= localStorage.getItem('File')? localStorage.getItem('File') : preview
 return(
-    <div className="App">
-        <h2>Display Preview Image</h2>
-        <input className="my-file" type="file" ref={ref} onChange={handleChange} />
-        {localStorage.getItem('File') && 
-            <img id="myDIV" className="preview-img" src={localStorage.getItem("File")} alt="Previw-Img" />
-         }
-        <button onClick={remove}>Removed Local</button>
-    </div>
+     <div className="App main-preview-page">
+     <TransformWrapper  initialScale={1}  initialPositionX={0}  initialPositionY={0}>
+       {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+         <React.Fragment>
+          {/* start Image  zoom control    */}
+          <div>
+           <button className="remove-image" onClick={remove }>Remove Image</button>
+             <div className="main-control">
+             <button onClick={() => zoomOut()}>-</button>
+             <button onClick={() => zoomIn()}>+</button>
+             <button onClick={() => resetTransform()}>Reset</button>
+           </div>
+           </div>
+          {/* end Image zoom control   */}
+
+
+  <TransformComponent className="zoom-image container">
+      <input className="my-file" type="file" ref={ref} onChange={handleChange} />
+
+  <img className="preview-img banner-img" src={image_path} alt="Previw-Img" />
+
+  </TransformComponent>
+
+    </React.Fragment>
+    )}
+    </TransformWrapper>
+  </div>
 );
 }
 export default PreviewImg;
